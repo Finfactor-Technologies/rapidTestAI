d5268efac23c5687671457e4659fdcf512990354
viveky <viveky@finfactor.in>
2025-05-08 14:15:10 +0530

    initial commit

---
+++ src/main/java/com/ftpl/rapidTestAI/RapidTestAiApplication.java
@@ -0,0 +1,13 @@
+package com.ftpl.rapidTestAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidTestAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidTestAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ftpl/rapidTestAI/RapidTestAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftpl.rapidTestAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidTestAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
